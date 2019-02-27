f073fa5338be60e4943a17db01529daa7f4e925c
Ewen McNeill <ewen@naos.co.nz>
2019-02-27 19:11:22 +1300

    openocd: Fix conda build issues
    Remove patches merged upstream (digilent-arty-s7, numato-mimas-a7,
    spartan7-idcodes).  Update build tooling dependencies to match
    upstream requirements (https://github.com/ntfreak/openocd/blob/master/README)
    using Conda packages to get new enough versions (especially pkg-config).
    Work around fallout of Conda pkg-config compile options.

--- openocd/build.sh
+++ openocd/build.sh
@@ -13,6 +13,21 @@ fi
 #export GIT_COMMITTER_EMAIL="$GIT_AUTHOR_EMAIL"
 #git commit -a -m "Changes for conda."
 
+# We need fairly new pkg-config, from conda, to be able to complete bootstrap
+# but we also need pkg-config to find the host OS package locations, so add
+# those into the pkg-config search path too.
+#
+export PKG_CONFIG_PATH=/usr/lib/x86_64-linux-gnu/pkgconfig
+
+# Conda's pkg-config is compiled with --enable-define-prefix, which
+# guesses the wrong prefix from the system .pc files (/usr/lib/include/...)
+# so we need a wrapper to inhibit that behaviour.
+#
+export PKG_CONFIG="${RECIPE_DIR}/pkg-config-no-prefix"
+
+# NOTE: We rely on building with the host OS gcc, etc, *not* Conda gcc,
+# in order that the host OS include paths, etc, are used (eg, for libusb).
+
 git status
 
 ./bootstrap
